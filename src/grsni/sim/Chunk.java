718c4425dccf605be41f497cc6ff534316242556
Santiago Mas <santiagojesus.mas@gmail.com>
2020-03-03 21:06:38 +0100

    Started work on tile colouring, needs refactoring

--- src/grsni/sim/Chunk.java
+++ src/grsni/sim/Chunk.java
@@ -5,32 +5,44 @@ import java.util.Arrays;
 import processing.core.*;
 
 public class Chunk {
+	int id;
 	public static final int CHUNK_SIZE = 16;
 	int xPos, yPos;
 	float[][] values;
-	OpenSimplexNoise valGenerator;
 	PApplet parent;
 
-	public Chunk(PApplet parent, OpenSimplexNoise gen, int i, int j) {
+	public Chunk(PApplet parent, int id, int i, int j) {
+		this.id = id;
 		this.parent = parent;
 		this.xPos = i;
 		this.yPos = j;
-		valGenerator = gen;
 		values = generateValues();
 	}
 
 	float[][] generateValues() {
+//		System.err.println("Chunk ID :" + id);
 		float[][] vals = new float[CHUNK_SIZE][CHUNK_SIZE];
 
 		for (int i = 0; i < CHUNK_SIZE; i++) {
 			for (int j = 0; j < CHUNK_SIZE; j++) {
 //				System.out.println(i + ": " + j + ", " + xPos + ": " + yPos);
-				float val = (float) valGenerator.eval((i + CHUNK_SIZE * xPos) * MapGenerator.NOISE_SCALE,
-						(j + CHUNK_SIZE * yPos) * MapGenerator.NOISE_SCALE) + 0.707f;
-				vals[i][j] = PApplet.map(val, 0, 1.414f, 0, 255);
+				float val = generateTileValue(i, j, xPos, yPos, 0, 0);
+				float variance = generateTileValue(i, j, xPos, yPos, 2000, 4000);
+				float humidity = generateTileValue(i, j, xPos, yPos, 8000, 10000);
+//				vals[i][j] = PApplet.map(val, 0, 1.415f, 0, 100);
+				vals[i][j] = PApplet.map(val, 0, 1, 0f, 100f);
+				vals[i][j]+=PApplet.map(variance, 0f, 1f, -20f, 20f);
 			}
+//			 System.err.println(Arrays.toString(vals[i]));
 		}
 		return vals;
+	}
+
+	float  generateTileValue(int xIndex, int yIndex, int xPos, int yPos, int xOffset, int yOffset) {
+		float xNoisePos = MapGenerator.NOISE_SCALE * ((xIndex + CHUNK_SIZE * xPos) + xOffset);
+		float yNoisePos = MapGenerator.NOISE_SCALE * ((yIndex + CHUNK_SIZE * yPos) + yOffset);
+//		return (float) (SimplexNoise.noise(xNoisePos, yNoisePos));
+		return parent.noise(xNoisePos, yNoisePos);
 	}
 
 	public float[][] getValues() {
--- src/grsni/sim/MapGenerator.java
+++ src/grsni/sim/MapGenerator.java
@@ -3,20 +3,28 @@ package grsni.sim;
 import processing.core.*;
 
 public class MapGenerator {
-	public static final float NOISE_SCALE = 0.05f;
-	public static final int CHUNK_NUMBER = 200;
+	public static final float NOISE_SCALE = 0.017f;
+	public static final int CHUNK_NUMBER = 60;
 
-	int scl = 2, mapWidth, mapHeight;
+	public static final float DARK_WATER_HEIGHT = 10;
+	public static final float WATER_HEIGHT = 15;
+	public static final float BEACH_HEIGHT = 33;
+	public static final float SAND_HEIGHT = 39;
+	public static final float GRASS_HEIGHT = 62;
+	public static final float ROCK_HEIGHT = 85;
+	public static final float SNOW_HEIGHT = 100;
+
+	public static final int TILE_SCALE = 1;
+
+	int mapWidth, mapHeight;
 	Chunk[][] terrain;
 	PApplet parent;
-	OpenSimplexNoise generator;
 
 	public MapGenerator(PApplet parent, int x, int h) {
 		this.parent = parent;
 		terrain = new Chunk[CHUNK_NUMBER][CHUNK_NUMBER];
 		mapWidth = x;
 		mapHeight = h;
-		generator = new OpenSimplexNoise(System.nanoTime());
 		generateMap();
 
 	}
@@ -25,7 +33,7 @@ public class MapGenerator {
 		terrain = new Chunk[CHUNK_NUMBER][CHUNK_NUMBER];
 		for (int i = 0; i < CHUNK_NUMBER; i++) {
 			for (int j = 0; j < CHUNK_NUMBER; j++) {
-				terrain[i][j] = new Chunk(parent, generator, i, j);
+				terrain[i][j] = new Chunk(parent, i * CHUNK_NUMBER + j, i, j);
 //				terrain[i][j].printValues();
 			}
 		}
@@ -44,15 +52,16 @@ public class MapGenerator {
 	}
 
 	void renderChunks(PGraphics map, int cornerX, int cornerY) {
-		int xStartIndex = cornerX / scl / 16;
-		int yStartIndex = cornerY / scl / 16;
-		int visibleChunksInX = PApplet.floor(mapWidth / scl / Chunk.CHUNK_SIZE) + 1;
-		int visibleChunksInY = PApplet.floor(mapHeight / scl / Chunk.CHUNK_SIZE) + 1;
+		int xStartIndex = cornerX / TILE_SCALE / 16;
+		int yStartIndex = cornerY / TILE_SCALE / 16;
+		int visibleChunksInX = PApplet.floor(mapWidth / TILE_SCALE / Chunk.CHUNK_SIZE) + 1;
+		int visibleChunksInY = PApplet.floor(mapHeight / TILE_SCALE / Chunk.CHUNK_SIZE) + 1;
 		for (int i = xStartIndex; i < xStartIndex + visibleChunksInX; i++) {
 			for (int j = yStartIndex; j < yStartIndex + visibleChunksInY; j++) {
 				if (i >= 0 && i < CHUNK_NUMBER && j >= 0 && j < CHUNK_NUMBER) {
 					Chunk chunk = terrain[i][j];
-					map.image(renderChunk(chunk, scl), i * Chunk.CHUNK_SIZE * scl, j * Chunk.CHUNK_SIZE * scl);
+					map.image(renderChunk(chunk, TILE_SCALE), i * Chunk.CHUNK_SIZE * TILE_SCALE,
+							j * Chunk.CHUNK_SIZE * TILE_SCALE);
 				}
 			}
 		}
@@ -65,7 +74,8 @@ public class MapGenerator {
 		chunkMap.noStroke();
 		for (int i = 0; i < Chunk.CHUNK_SIZE; i++) {
 			for (int j = 0; j < Chunk.CHUNK_SIZE; j++) {
-				chunkMap.fill(c.getValues()[i][j]);
+				int heightColor = getTileColor(c.getValues()[i][j]);
+				chunkMap.fill(heightColor);
 				chunkMap.square(i * scl, j * scl, scl);
 			}
 		}
@@ -74,21 +84,40 @@ public class MapGenerator {
 			chunkMap.stroke(255, 0, 0);
 			chunkMap.square(0, 0, Chunk.CHUNK_SIZE * scl);
 			chunkMap.fill(255, 0, 0);
+			chunkMap.textAlign(PApplet.CENTER);
 			chunkMap.text(c.xPos + ", " + c.yPos, Chunk.CHUNK_SIZE / 2 * scl, Chunk.CHUNK_SIZE / 2 * scl);
 		}
 		chunkMap.endDraw();
 		return chunkMap;
 	}
 
+	int getTileColor(float h) {
+		int color = 0xffff0000;
+		if (h < WATER_HEIGHT) {
+			color = COLORS.SEA;
+		} else if (h < BEACH_HEIGHT) {
+			color = COLORS.BEACH;
+		} else if (h < SAND_HEIGHT) {
+			color = COLORS.SAND;
+		} else if (h < GRASS_HEIGHT) {
+			color = COLORS.GRASS;
+		} else if (h < ROCK_HEIGHT) {
+			color = COLORS.ROCK;
+		} else if (h < SNOW_HEIGHT) {
+			color = COLORS.SNOW;
+		}
+		return color;
+	}
+
 }
 
 class COLORS {
-	public final int SEA = 0xff0d1ed3;
-	public final int BEACH = 0xff3ed8ea;
-	public final int SAND = 0xffe8f088;
-	public final int GRASS = 0xff0ebf06;
-	public final int TREE = 0xff115d03;
-	public final int ROCK = 0xffbbbbbb;
-	public final int SNOW = 0xffffffff;
+	public static final int SEA = 0xff0d63d3;
+	public static final int BEACH = 0xff3adeb5;
+	public static final int SAND = 0xffdfeb63;
+	public static final int GRASS = 0xff44b81a;
+	public static final int TREE = 0xff115d03;
+	public static final int ROCK = 0xffbbbbbb;
+	public static final int SNOW = 0xffffffff;
 
 }
